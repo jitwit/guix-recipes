b431ac6c9600876bc84b90d526b4b379d5aaaabc
Joseph Novakovich <jrn@bluefarm.ca>
2020-08-27 16:35:31 -0400

    update

--- jrn/packages/j.scm
+++ jrn/packages/j.scm
@@ -45,11 +45,11 @@
      (origin
        (method git-fetch)
        (uri
-	(git-reference
-	 (url "https://github.com/jsoftware/jsource.git")
-	 (commit "7042acfbe298d8bb06874d70a6043aa62f2a43b9")))
+        (git-reference
+         (url "https://github.com/jsoftware/jsource.git")
+         (commit "59324abbc6c9c3709d39096f5a41e0a4ef28e9f6")))
        (sha256
-	(base32 "017vcs1yrzm4iqhx0mv3c8mgbscj4980i0rwzy8b327bxjznkjfd"))))
+        (base32 "031mncgbnn89s9k34aml7jn9pr3cfmvfm06v8785br483jmdmn20"))))
     (build-system gnu-build-system)
     (inputs
      `(("bash" ,bash)
@@ -62,78 +62,85 @@
     (arguments
      `(#:modules
        ((guix build gnu-build-system)
-	(guix build utils))
+        (guix build utils))
        #:phases
        (modify-phases %standard-phases
-	 (replace 'configure
-	   (lambda* (#:key inputs outputs #:allow-other-keys)
-	     (let* ((jplatform ,(if (target-arm?) "raspberry" "linux"))
-		    (out     (assoc-ref %outputs "out")))
-	       (with-output-to-file "jsrc/jversion.h"
-		 (lambda ()
-		   (display "#define jversion  ") (write ,version)  (newline)
-		   (display "#define jplatform ") (write jplatform) (newline)
-		   (display "#define jtype     ") (write "beta")    (newline)
-		   (display "#define jlicense  ") (write "GPL3")    (newline)
-		   (display "#define jbuilder  ") (write "guix.gnu.org")
-		   (newline)))
-	       (substitute* `("jlibrary/system/main/regex.ijs")
-		 (("pcre2dll=: f")
-		  (string-append "pcre2dll=: '"
-				 (assoc-ref %build-inputs "pcre2")
-				 "/lib/libpcre2-8.so.0'")))
-	       (substitute* `("jlibrary/system/util/tar.ijs")
-		 (("libz=: .+$")
-		  (string-append "zlib=: '"
-				 (assoc-ref %build-inputs "zlib")
-				 "/lib/libz.so'\n")))
-	       #t)))
-	 (replace 'build
-	   (lambda* (#:key inputs outputs #:allow-other-keys)
-	     (let ((jplatform ,(if (target-arm?) "raspberry" "linux"))
-		   (j64x ,(if (target-64bit?) "j64avx2" "j32")))
-	       (chdir "make2")
-	       (system (format #f "jplatform=~a j64x=~a USE_SLEEF=1 ./build_jconsole.sh"
-			       jplatform j64x))
-	       (system (format #f "jplatform=~a j64x=~a USE_SLEEF=1 ./build_tsdll.sh"
-			       jplatform j64x))
-	       (system (format #f "jplatform=~a j64x=~a USE_SLEEF=1 ./build_libj.sh"
-			       jplatform j64x))
-	       (chdir "..")
-	       #t)))
-	 (replace 'check
-	   (lambda* (#:key inputs outputs #:allow-other-keys)
-	     (let* ((jplatform ,(if (target-arm?) "raspberry" "linux"))
-		    (j64x ,(if (target-64bit?) "j64avx2" "j32"))
-		    (tsu (string-append (getcwd) "/test/tsu.ijs"))
-		    (jbld (string-append "bin/" jplatform "/" j64x)))
-	       ;; following directions in make2/make.txt
-	       (copy-recursively jbld "jlibrary/bin")
-	       (chdir "jlibrary/bin")
-	       (system "echo \"RUN ddall\" | ./jconsole ../../test/tsu.ijs")
-	       (chdir "../..")
-	       #t)))
-	 (replace 'install
- 	   (lambda* (#:key inputs outputs #:allow-other-keys)
-	     (let* ((bin-out (string-append (assoc-ref %outputs "out") "/bin"))
-		    (share-out (string-append (assoc-ref %outputs "out")
-					      "/share/j"))
-		    (jconsole "jlibrary/bin/jconsole")
-		    (libj.so  "jlibrary/bin/libj.so"))
-	       (install-file jconsole bin-out)
-	       (install-file libj.so bin-out)
-	       (copy-recursively "jlibrary/addons"
-				 (string-append share-out "/addons"))
-	       (copy-recursively "jlibrary/system"
-				 (string-append share-out "/system"))
-	       ;; rewrite of J's usual profile.ijs to play nice with
-	       ;; guix. going through $HOME/.guix-profile/share/j
-	       ;; isn't great, but it allows us to install addons
-	       ;; independently
-	       (with-output-to-file (string-append bin-out "/profile.ijs")
-		 (lambda ()
-		   (display
-		    "NB. J profile
+         (replace 'configure
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((jplatform "linux")
+                    (out (assoc-ref %outputs "out")))
+               (with-output-to-file "jsrc/jversion.h"
+                 (lambda ()
+                   (display "#define jversion  ") (write ,version)  (newline)
+                   (display "#define jplatform ") (write jplatform) (newline)
+                   (display "#define jtype     ") (write "beta")    (newline)
+                   (display "#define jlicense  ") (write "GPL3")    (newline)
+                   (display "#define jbuilder  ") (write "guix.gnu.org")
+                   (newline)))
+               (substitute* `("jlibrary/system/main/regex.ijs")
+                 (("pcre2dll=: f")
+                  (string-append "pcre2dll=: '"
+                                 (assoc-ref %build-inputs "pcre2")
+                                 "/lib/libpcre2-8.so.0'")))
+               (substitute* `("jlibrary/system/util/tar.ijs")
+                 (("libz=: .+$")
+                  (string-append "zlib=: '"
+                                 (assoc-ref %build-inputs "zlib")
+                                 "/lib/libz.so'\n")))
+               #t)))
+         (replace 'build
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let ((jplatform "linux")
+                   (j64x "j64avx2"))
+               (chdir "make2")
+               (system
+                (format #f
+                        "jplatform=~a j64x=~a USE_SLEEF=1 ./build_jconsole.sh"
+                        jplatform j64x))
+               (system
+                (format #f
+                        "jplatform=~a j64x=~a USE_SLEEF=1 ./build_tsdll.sh"
+                        jplatform j64x))
+               (system
+                (format #f
+                        "jplatform=~a j64x=~a USE_SLEEF=1 ./build_libj.sh"
+                        jplatform
+                        j64x))
+               (chdir "..")
+               #t)))
+         (replace 'check
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((jplatform "linux")
+                    (j64x "j64avx2")
+                    (tsu (string-append (getcwd) "/test/tsu.ijs"))
+                    (jbld (string-append "bin/" jplatform "/" j64x)))
+               ;; following directions in make2/make.txt
+               (copy-recursively jbld "jlibrary/bin")
+               (chdir "jlibrary/bin")
+               (system "echo \"RUN ddall\" | ./jconsole ../../test/tsu.ijs")
+               (chdir "../..")
+               #t)))
+         (replace 'install
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((bin-out (string-append (assoc-ref %outputs "out") "/bin"))
+                    (share-out (string-append (assoc-ref %outputs "out")
+                                              "/share/j"))
+                    (jconsole "jlibrary/bin/jconsole")
+                    (libj.so  "jlibrary/bin/libj.so"))
+               (install-file jconsole bin-out)
+               (install-file libj.so bin-out)
+               (copy-recursively "jlibrary/addons"
+                                 (string-append share-out "/addons"))
+               (copy-recursively "jlibrary/system"
+                                 (string-append share-out "/system"))
+               ;; custom profile.ijs to play nice with guix
+               ;; locations. going through $HOME/.guix-profile/share/j
+               ;; isn't great, but it allows us to include addons
+               ;; independently of J
+               (with-output-to-file (string-append bin-out "/profile.ijs")
+                 (lambda ()
+                   (display
+                    "NB. J profile
 NB. JFE sets BINPATH_z_ and ARGV_z_
 
 jpathsep_z_=: '/'&(('\\' I.@:= ])})
@@ -160,7 +167,6 @@ NB. used to create mutable j user directories for temp
 NB. files/configuring jqt/projects and so on
 md=. 3 : 0 NB. recursive makedir
 a=. jpathsep y,'/'
-if. ('root'-:2!:5'USER') +. ('//'-:2{.a)+.('/root/'-:6{.a)+.('/var/root/'-:10{.a)+.('/usr/'-:5{.a)+.('/tmp'-:a) do. return. end. NB. installed under / /root /usr
 if. -.#1!:0 }:a do.
   for_n. I. a='/' do. 1!:5 :: [ <n{.a end.
 end.
@@ -174,11 +180,13 @@ md snap
 md temp
 
 NB. boot up J and load startup.ijs if it exists
-0!:0 <jpathsep (4!:55 (;:'isroot userx ids md'), ids)]system,'/util/boot.ijs'\n")))
-	       #t))))))
+0!:0 <jpathsep (4!:55 (;:'isroot userx ids md'), ids)]system,'/util/boot.ijs'
+")))
+               #t))))))
     (synopsis "APL Dialect")
-    (description "Terse, interpreted, array language originally developed by
-Ken Iverson and Roger Hui.")
+    (description "J is a programming language that works with arrays,
+verbs, adverbs, and conjunctions.  For example, +/x sums array x and
+/:~x sorts it.")
     (home-page "https://code.jsoftware.com/wiki/Main_Page")
     (license gpl3)))
 
@@ -198,19 +206,17 @@ Ken Iverson and Roger Hui.")
     (build-system gnu-build-system)
     (outputs '("out"))
     (inputs `(("bash" ,bash)
-	      ("j" ,j)
+              ("j" ,j)
               ("mesa" ,mesa)
               ("pulseaudio" ,pulseaudio)
-	      ;; fixme figure out which of these isn't needed
               ("qtbase" ,qtbase)
               ("qtwebsockets" ,qtwebsockets)
               ("qtsvg" ,qtsvg)
-	      ;; ("qtwebkit" ,qtwebkit)
               ("qtdeclarative" ,qtdeclarative)
-	      ("qtquickcontrols" ,qtquickcontrols)
+              ("qtquickcontrols" ,qtquickcontrols)
               ("qtwebchannel" ,qtwebchannel)
               ("qtmultimedia" ,qtmultimedia)
-	      ("qtwebengine" ,qtwebengine)))
+              ("qtwebengine" ,qtwebengine)))
     (arguments
      `(#:modules ((guix build gnu-build-system) (guix build utils))
        #:tests? #f
@@ -227,7 +233,7 @@ Ken Iverson and Roger Hui.")
              (copy-recursively "bin/linux-x86_64/release"
                                (string-append (assoc-ref %outputs "out")
                                               "/bin"))
-	     #t))
+             #t))
          (add-after 'install 'wrap-executable
            (lambda* (#:key inputs outputs #:allow-other-keys)
              (wrap-program (string-append (assoc-ref outputs "out") "/bin/jqt")
@@ -236,28 +242,27 @@ Ken Iverson and Roger Hui.")
                          (string-append (assoc-ref inputs label)
                                         "/lib/qt5/plugins"))
                        '("qtbase"
-			 "qtdeclarative"
-			 "qtwebsockets"
-			 ;; "qtwebkit"
-			 "qtsvg"
-			 "qtwebengine"
-			 "qtquickcontrols"
-			 "qtmultimedia"
-			 "qtwebchannel")))
-	       `("QTWEBENGINEPROCESS_PATH" =
-		 (,(string-append  (assoc-ref inputs "qtwebengine")
-				   "/lib/qt5/libexec/QtWebEngineProcess"))))
-	     (substitute* `(,(string-append (assoc-ref outputs "out") "/bin/jqt"))
-	       (("\\$@")
-		(string-append "-lib\" \""
-			       (assoc-ref inputs "j") "/bin/libj.so\" "
-			       "\"-jprofile\" \""
-			       (assoc-ref inputs "j") "/bin/profile.ijs\" "
-			       "\"$@")))
+                         "qtdeclarative"
+                         "qtwebsockets"
+                         "qtsvg"
+                         "qtwebengine"
+                         "qtquickcontrols"
+                         "qtmultimedia"
+                         "qtwebchannel")))
+               `("QTWEBENGINEPROCESS_PATH" =
+                 (,(string-append  (assoc-ref inputs "qtwebengine")
+                                   "/lib/qt5/libexec/QtWebEngineProcess"))))
+             (substitute* `(,(string-append (assoc-ref outputs "out") "/bin/jqt"))
+               (("\\$@")
+                (string-append "-lib\" \""
+                               (assoc-ref inputs "j") "/bin/libj.so\" "
+                               "\"-jprofile\" \""
+                               (assoc-ref inputs "j") "/bin/profile.ijs\" "
+                               "\"$@")))
              #t)))))
-    (synopsis "The jqtide application is an executable, jqt, and a
-shared object, libjqt.so")
-    (description "The jqtide application is an executable, jqt, and a
-shared object, libjqt.so.")
+    (synopsis "The jqtide application is an executable jqt, and a
+shared object libjqt.so")
+    (description "The jqtide application is an executable jqt, and a
+shared object libjqt.so.")
     (home-page "https://code.jsoftware.com/wiki/Main_Page")
     (license lgpl2.1)))
